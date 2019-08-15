db34076f37730acc8ba9256777cee40d5733c2dc
Faturrahman <faturrahmanmakruf@gmail.com>
2019-08-15 16:04:26 +0700

    deploy with mysql is done

--- crawl.py
+++ crawl.py
@@ -2,39 +2,58 @@ from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
+import pymysql
+
 options = Options()
 options.headless = True
-driver = webdriver.Chrome(options=options)
-#driver.get("https://www.youtube.com/")
-#element_text = driver.find_element_by_id("title").text
-driver.get("https://play.google.com/store/apps/details?id=com.akupintar.mobile.siswa&showAllReviews=true")
-lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-# action = webdriver.common.action_chains.ActionChains(driver)
+browser = webdriver.Chrome(options=options)
+
+db = pymysql.connect('localhost', 'root', '', 'crawl')
+cursor = db.cursor()
+
+drop = """DROP TABLE IF EXISTS REVIEW"""
+create = """CREATE TABLE REVIEW (
+    id int,
+    name char(40),
+    rating int,
+    date varchar(255),
+    comment text ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;"""
+
+try:
+    cursor.execute(create)
+except:
+    cursor.execute(drop)
+    cursor.execute(create)
+
+browser.get("https://play.google.com/store/apps/details?id=com.akupintar.mobile.siswa&showAllReviews=true")
+lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 match=False
 while(match==False):
         lastCount = lenOfPage
         time.sleep(3)
-        lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
         if lastCount==lenOfPage:
             time.sleep(2)
-            showMore = driver.find_element_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div")
-            driver.execute_script("arguments[0].click();", showMore)
-            # action.move_to_element_with_offset(showMore, 612, 18)
-            # action.click()
-            # action.perform()
+            showMore = browser.find_element_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div")
+            browser.execute_script("arguments[0].click();", showMore)
             time.sleep(4)
             match=True
 
-reviews = driver.find_elements_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[@jscontroller='H6eOGe']")
+reviews = browser.find_elements_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[@jscontroller='H6eOGe']")
 
 id_review = 1
 for review in reviews:
    soup = BeautifulSoup(review.get_attribute("innerHTML"), "lxml")
-#    print (soup)
    name = soup.find(class_="X43Kjb").text
    rating = soup.find('div',role='img').get('aria-label').strip("Rated ")[0]
    date = soup.find(class_="p2TkOb").text
    comment = soup.find(class_="UD7Dzf").text
    comment = comment.lstrip()
-   print (name)
-#print(element_text)
+   insert_query = """INSERT INTO REVIEW(id, name, rating, date, comment) values (%s, %s, %s, %s, %s)"""
+   insert_data = (id_review, name, rating, date, comment)
+   result = cursor.execute(insert_query, insert_data)
+   db.commit()
+   id_review += 1
+
+db.close()
+browser.quit() 
--- headless.py
+++ headless.py
@@ -1,85 +1,73 @@
-# import pymysql
-# import time
-# from selenium import webdriver
-# from bs4 import BeautifulSoup
-# from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-# db = pymysql.connect('localhost', 'pedro', '1', 'python')
-
-# cursor = db.cursor()
-
-# drop = """DROP TABLE IF EXISTS REVIEW"""
-# create = """CREATE TABLE REVIEW (
-#     id int,
-#     name char(40),
-#     rating int,
-#     date varchar(255),
-#     comment text ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;"""
-
-# try:
-#     cursor.execute(create)
-# except:
-#     cursor.execute(drop)
-#     cursor.execute(create)
-
-# cap = DesiredCapabilities().FIREFOX
-# cap["marionette"] = True
-# options = Options()
-# options.headless = True
-# browser = webdriver.Firefox(options=options, capabilities=cap)
-# # browser = webdriver.Firefox(options=options, capabilities=cap)
-# # ids = []
-# # names = []
-# # ratings = []
-# # dates = []
-# # comments = []
-
-# browser.get("https://play.google.com/store/apps/details?id=com.akupintar.mobile.siswa&showAllReviews=true")
-
-# # lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-# # match=False
-# # while(match==False):
-# #         lastCount = lenOfPage
-# #         time.sleep(3)
-# #         lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-# #         if lastCount==lenOfPage:
-# #             showMore = browser.find_element_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div").click()
-# #             match=True
-
-# reviews = browser.find_elements_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[@jscontroller='H6eOGe']")
-
-# id_review = 1
-# for review in reviews:
-#     soup = BeautifulSoup(review.get_attribute("innerHTML"),"lxml")
-#     name = soup.find(class_="X43Kjb").text
-#     rating = soup.find('div',role='img').get('aria-label').strip("Rated ")[0]
-#     date = soup.find(class_="p2TkOb").text
-#     comment = soup.find(class_="UD7Dzf").text
-#     comment = comment.lstrip()
-#     # names.append(name)
-#     # ratings.append(rating)
-#     # dates.append(date)
-#     # comments.append(comment)
-#     # ids.append(id_review)
-#     insert_query = """INSERT INTO REVIEW(id, name, rating, date, comment) values (%s, %s, %s, %s, %s)"""
-#     insert_tuple = (id_review, name, rating, date, comment)
-#     result = cursor.execute(insert_query, insert_tuple)
-#     db.commit()
-#     id_review += 1
-
-# db.close()
-# print ("Headless Firefox Initialized")
-# browser.quit()
-
-
+import pymysql
+import time
 from selenium import webdriver
-service = webdriver.chrome.service.Service('./chromedriver')
-service.start()  
-options = webdriver.ChromeOptions()                
-options.add_argument('--headless')
-options = options.to_capabilities()
-driver = webdriver.Remote(service.service_url, options)
-driver.get("https://play.google.com/store/apps/details?id=com.akupintar.mobile.siswa&showAllReviews=true")
-reviews = driver.find_elements_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[@jscontroller='H6eOGe']")
-print (reviews)
+from bs4 import BeautifulSoup
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+db = pymysql.connect('localhost', 'pedro', '1', 'python')
+
+cursor = db.cursor()
+
+drop = """DROP TABLE IF EXISTS REVIEW"""
+create = """CREATE TABLE REVIEW (
+    id int,
+    name char(40),
+    rating int,
+    date varchar(255),
+    comment text ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;"""
+
+try:
+    cursor.execute(create)
+except:
+    cursor.execute(drop)
+    cursor.execute(create)
+
+cap = DesiredCapabilities().FIREFOX
+cap["marionette"] = True
+options = Options()
+options.headless = True
+browser = webdriver.Firefox(options=options, capabilities=cap)
+# browser = webdriver.Firefox(options=options, capabilities=cap)
+# ids = []
+# names = []
+# ratings = []
+# dates = []
+# comments = []
+
+browser.get("https://play.google.com/store/apps/details?id=com.akupintar.mobile.siswa&showAllReviews=true")
+
+# lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+# match=False
+# while(match==False):
+#         lastCount = lenOfPage
+#         time.sleep(3)
+#         lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+#         if lastCount==lenOfPage:
+#             showMore = browser.find_element_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div").click()
+#             match=True
+
+reviews = browser.find_elements_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[@jscontroller='H6eOGe']")
+
+id_review = 1
+for review in reviews:
+    soup = BeautifulSoup(review.get_attribute("innerHTML"),"lxml")
+    name = soup.find(class_="X43Kjb").text
+    rating = soup.find('div',role='img').get('aria-label').strip("Rated ")[0]
+    date = soup.find(class_="p2TkOb").text
+    comment = soup.find(class_="UD7Dzf").text
+    comment = comment.lstrip()
+    # names.append(name)
+    # ratings.append(rating)
+    # dates.append(date)
+    # comments.append(comment)
+    # ids.append(id_review)
+    insert_query = """INSERT INTO REVIEW(id, name, rating, date, comment) values (%s, %s, %s, %s, %s)"""
+    insert_tuple = (id_review, name, rating, date, comment)
+    result = cursor.execute(insert_query, insert_tuple)
+    db.commit()
+    id_review += 1
+
+db.close()
+print ("Headless Firefox Initialized")
+browser.quit()
