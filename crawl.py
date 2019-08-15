70e366dad5436f3023be4a57354c563c1d514b07
Faturrahman <faturrahmanmakruf@gmail.com>
2019-08-15 17:33:40 +0700

    minor update

--- crawl.py
+++ crawl.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import initheadless
 import initdb
 
+def scroll():
+    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    return lenOfPage
+
 browser = initheadless.headless_browser()
 
 db = initdb.create_db()
@@ -10,13 +14,13 @@ cursor = initdb.get_cursor(db)
 
 browser.get("https://play.google.com/store/apps/details?id=com.akupintar.mobile.siswa&showAllReviews=true")
 
-lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-match=False
+match = False
+print('Loading page...')
 while(match==False):
-        lastCount = lenOfPage
+        lastCount = scroll()
         time.sleep(3)
-        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount==lenOfPage:
+        scroll()
+        if lastCount == scroll():
             time.sleep(2)
             showMore = browser.find_element_by_xpath("//div[1]/div[4]/c-wiz/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div")
             browser.execute_script("arguments[0].click();", showMore)
